f1b7bcba38b0c82f9e7d71da5c377de757e8ff4c
jicmu <jicmu0@gmail.com>
2023-10-04 20:59:27 +0900

    Feat: 지도, 마킹 함수 생성

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,19 @@
-<html>
+<%@ page contentType="text/html; charset=UTF-8" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Title</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+    <div id="map" style="width:500px;height:400px;"></div>
 </body>
+    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey="></script>
+    <script type="text/javascript" src="resources/js/index.js"></script>
+
+    <script>
+        let map = drawMap(33.450701, 126.570667);
+
+        markMap(33.450701, 126.570667, map);
+    </script>
 </html>
---
+++ src/main/webapp/resources/js/index.js
@@ -0,0 +1,30 @@
+let drawMap = (latitude, longitude) => {
+    var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    var options = { //지도를 생성할 때 필요한 기본 옵션
+        center: new kakao.maps.LatLng(latitude, longitude), //지도의 중심좌표.
+        level: 5 //지도의 레벨(확대, 축소 정도)
+    };
+
+    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+    return map;
+}
+
+
+let markMap = (latitude, longitude, map) => {
+    var position =  new kakao.maps.LatLng(latitude, longitude);
+
+    var marker = new kakao.maps.Marker({
+      position: position,
+      clickable: true // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
+    });
+
+    marker.setMap(map);
+}
+
+// positions는 객체 { latitue, longitude } 배열
+let multiMarkMap = (positions, map) {
+    for (let position of positions) {
+        markMap(position.latitude, position.longitude, map);
+    }
+}
