30452790c947d43e0f5c11ed7b6b51d2416640b1
jicmu <jicmu0@gmail.com>
2023-10-05 11:43:32 +0900

    Feat: 고속도로 코드로 휴게소 표시

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,42 +8,12 @@
 <body>
     <div id="map" style="width:500px;height:400px;"></div>
 </body>
-    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey="></script>
+    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=&libraries=services,clusterer,drawing"></script>
+
     <script type="text/javascript" src="resources/js/index.js"></script>
 
     <script>
-        let map = drawMap(33.450701, 126.570667);
-
-        let url = "http://data.ex.co.kr/openapi/locationinfo/locationinfoRest?key=&type=json";
-        let xhr = new XMLHttpRequest();
-
-        xhr.open("get", url);
-
-        xhr.send();
-
-        xhr.onload = () => {
-            if (xhr.status != 200) {
-                alert("오류!");
-                return;
-            }
-
-            let result = JSON.parse(xhr.response);
-
-            let positions = [];
-            for (let r of result.list) {
-                let position = {
-                    latitude: r.yValue,
-                    longitude: r.xValue,
-                };
-
-                positions.push(position);
-            }
-
-            multiMarkMap(positions, map);
-        }
-
-
-
-        markMap(33.450701, 126.570667, map);
+        let map = drawMap(37.5642135, 127.0016985);
+        markByRouteCd("", "0010");
     </script>
 </html>
--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -8,10 +8,9 @@ let drawMap = (latitude, longitude) => {
     var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
     return map;
-};
+}
 
-
-let markMap = (latitude, longitude, map) => {
+let markMap = (latitude, longitude) => {
     var position =  new kakao.maps.LatLng(latitude, longitude);
 
     var marker = new kakao.maps.Marker({
@@ -23,8 +22,56 @@ let markMap = (latitude, longitude, map) => {
 };
 
 // positions는 객체 { latitue, longitude } 배열
-let multiMarkMap = (positions, map) => {
+let multiMarkMap = (positions) => {
     for (let position of positions) {
         markMap(position.latitude, position.longitude, map);
     }
 };
+
+// 주소로 좌표 알아냄
+let markByAddr = (addr) => {
+    // 주소-좌표 변환 객체를 생성합니다
+    var geocoder = new kakao.maps.services.Geocoder();
+
+    // 주소로 좌표를 검색합니다
+    geocoder.addressSearch(addr, function(result, status) {
+        // 정상적으로 검색이 완료됐으면
+         if (status === kakao.maps.services.Status.OK) {
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+            var marker = new kakao.maps.Marker({
+                map: map,
+                position: coords,
+            });
+
+            marker.setMap(map);
+        }
+    });
+};
+
+
+let markByRouteCd = (exKey, routeCd) => {
+    locationInfoUrl = "http://data.ex.co.kr/openapi/restinfo/hiwaySvarInfoList";
+    locationInfoUrl += "?key=" + exKey + "&'type=json";
+    locationInfoUrl += "&routeCd=" + routeCd;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("get", locationInfoUrl);
+
+    xhr.send();
+
+    xhr.onload = () => {
+        if (xhr.status != 200) {
+            alert("오류!");
+            return;
+        }
+
+        let result = JSON.parse(xhr.response);
+
+        let positions = [];
+        for (let r of result.list) {
+            markByAddr(r.svarAddr);
+        }
+    }
+}
